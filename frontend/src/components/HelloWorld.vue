178d4dfb804df7d74099da4e973354ffef9c8279
xiaoyu_jie <kclon29@gmail.com>
2026-04-03 12:34:09 +0800

    SAM

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,8 +1,5 @@
 <script setup>
 import { ref } from 'vue'
-import viteLogo from '../assets/vite.svg'
-import heroImg from '../assets/hero.png'
-import vueLogo from '../assets/vue.svg'
 
 const count = ref(0)
 </script>
@@ -11,8 +8,6 @@ const count = ref(0)
   <section id="center">
     <div class="hero">
       <img :src="heroImg" class="base" width="170" height="179" alt="" />
-      <img :src="vueLogo" class="framework" alt="Vue logo" />
-      <img :src="viteLogo" class="vite" alt="Vite logo" />
     </div>
     <div>
       <h1>Get started</h1>
